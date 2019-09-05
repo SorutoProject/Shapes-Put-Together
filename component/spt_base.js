e166d7d61e84123b84f64e4ead1133981ac383ec
SorutoProject <32834893+SorutoProject@users.noreply.github.com>
2019-09-05 16:37:02 +0900

    Update spt_base.js

--- component/spt_base.js
+++ component/spt_base.js
@@ -4,31 +4,37 @@ sound = {
     "bubble": new Howl({
         src: ["./component/sound/bubble-burst1.mp3"],
         preload: true,
+        volume:0.4,
         html5: true
     }),
     "bound": new Howl({
         src: ["./component/sound/table-tennis-ball1.mp3"],
         preload: true,
+        volume:0.4,
         html5: true
     }),
     "clear": new Howl({
         src: ["./component/sound/decision27.mp3"],
         preload: true,
+        volume:0.4,
         html5: true
     }),
     "decision": new Howl({
         src: ["./component/sound/decision23.mp3"],
         preload: true,
+        volume:0.4,
         html5: true
     }),
     "cancel": new Howl({
         src: ["./component/sound/cancel4.mp3"],
         preload: true,
+        volume:0.4,
         html5: true
     }),
     "warning": new Howl({
         src: ["./component/sound/warning1.mp3"],
         preload: true,
+        volume:0.4,
         html5: true
     }),
     "bgm": {
@@ -37,6 +43,7 @@ sound = {
     "finish": new Howl({
         src:["./component/sound/trumpet2.mp3"],
         preload: true,
+        volume:0.4,
         html5: true
     })
     
